3abe2690886a0329bc3e291bf88602e42e7c9b7e
bryan <bryan@BRYAN>
2025-11-27 09:36:40 -0500

    cambios de formato wa 3

--- AgroRobotView/frmMantHeces.h
+++ AgroRobotView/frmMantHeces.h
@@ -303,9 +303,10 @@ namespace AgroRobotView {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(399, 358);
+			this->button4->Location = System::Drawing::Point(532, 441);
+			this->button4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->Size = System::Drawing::Size(100, 28);
 			this->button4->TabIndex = 22;
 			this->button4->Text = L"Eliminar";
 			this->button4->UseVisualStyleBackColor = true;
@@ -313,9 +314,10 @@ namespace AgroRobotView {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(169, 358);
+			this->button3->Location = System::Drawing::Point(225, 441);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->Size = System::Drawing::Size(100, 28);
 			this->button3->TabIndex = 21;
 			this->button3->Text = L"Editar";
 			this->button3->UseVisualStyleBackColor = true;
@@ -328,10 +330,11 @@ namespace AgroRobotView {
 				this->Column6,
 					this->Column1, this->Column2, this->Column3, this->Column4, this->Column5, this->Fecha
 			});
-			this->dataGridView1->Location = System::Drawing::Point(16, 147);
+			this->dataGridView1->Location = System::Drawing::Point(21, 181);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->Size = System::Drawing::Size(595, 183);
+			this->dataGridView1->Size = System::Drawing::Size(793, 225);
 			this->dataGridView1->TabIndex = 19;
 			// 
 			// Column6
@@ -389,18 +392,22 @@ namespace AgroRobotView {
 			this->groupBox1->Controls->Add(this->button1);
 			this->groupBox1->Controls->Add(this->textBox1);
 			this->groupBox1->Controls->Add(this->label1);
-			this->groupBox1->Location = System::Drawing::Point(42, 23);
+			this->groupBox1->Location = System::Drawing::Point(56, 28);
+			this->groupBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(318, 100);
+			this->groupBox1->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->groupBox1->Size = System::Drawing::Size(424, 123);
 			this->groupBox1->TabIndex = 18;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Criterio de Búsqueda";
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &frmMantHeces::groupBox1_Enter);
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(85, 71);
+			this->button5->Location = System::Drawing::Point(113, 87);
+			this->button5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(137, 23);
+			this->button5->Size = System::Drawing::Size(135, 36);
 			this->button5->TabIndex = 31;
 			this->button5->Text = L"Limpiar";
 			this->button5->UseVisualStyleBackColor = true;
@@ -408,9 +415,10 @@ namespace AgroRobotView {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(210, 35);
+			this->button1->Location = System::Drawing::Point(280, 43);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(100, 36);
 			this->button1->TabIndex = 4;
 			this->button1->Text = L"Buscar";
 			this->button1->UseVisualStyleBackColor = true;
@@ -418,17 +426,19 @@ namespace AgroRobotView {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(67, 35);
+			this->textBox1->Location = System::Drawing::Point(89, 43);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(132, 22);
 			this->textBox1->TabIndex = 1;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(6, 38);
+			this->label1->Location = System::Drawing::Point(8, 47);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(48, 13);
+			this->label1->Size = System::Drawing::Size(60, 16);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Especie:";
 			// 
@@ -437,9 +447,11 @@ namespace AgroRobotView {
 			this->groupBox2->Controls->Add(this->label2);
 			this->groupBox2->Controls->Add(this->comboBox1);
 			this->groupBox2->Controls->Add(this->button2);
-			this->groupBox2->Location = System::Drawing::Point(399, 23);
+			this->groupBox2->Location = System::Drawing::Point(532, 28);
+			this->groupBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(200, 100);
+			this->groupBox2->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->groupBox2->Size = System::Drawing::Size(267, 123);
 			this->groupBox2->TabIndex = 23;
 			this->groupBox2->TabStop = false;
 			this->groupBox2->Text = L"Nueva Muestra Heces";
@@ -447,9 +459,10 @@ namespace AgroRobotView {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(30, 35);
+			this->label2->Location = System::Drawing::Point(40, 43);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(48, 13);
+			this->label2->Size = System::Drawing::Size(60, 16);
 			this->label2->TabIndex = 28;
 			this->label2->Text = L"Especie:";
 			this->label2->Click += gcnew System::EventHandler(this, &frmMantHeces::label2_Click);
@@ -457,16 +470,18 @@ namespace AgroRobotView {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(93, 32);
+			this->comboBox1->Location = System::Drawing::Point(124, 39);
+			this->comboBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(77, 21);
+			this->comboBox1->Size = System::Drawing::Size(101, 24);
 			this->comboBox1->TabIndex = 27;
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(33, 71);
+			this->button2->Location = System::Drawing::Point(44, 87);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(137, 23);
+			this->button2->Size = System::Drawing::Size(183, 28);
 			this->button2->TabIndex = 26;
 			this->button2->Text = L"Agregar";
 			this->button2->UseVisualStyleBackColor = true;
@@ -474,14 +489,15 @@ namespace AgroRobotView {
 			// 
 			// frmMantHeces
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(635, 406);
+			this->ClientSize = System::Drawing::Size(847, 500);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->groupBox1);
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"frmMantHeces";
 			this->Text = L"AgroRobot - Gestión de Muestras de Heces";
 			this->Load += gcnew System::EventHandler(this, &frmMantHeces::frmMantHeces_Load);
@@ -680,5 +696,7 @@ namespace AgroRobotView {
 		nuevaMuestraHeces->ShowDialog();
 		cargarAnimalesDisponibles();
 	}
+private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
