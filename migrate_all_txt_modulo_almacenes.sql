6b70c93118bac444d6b8450bb8f23dd228bc0d47
Becquer Isaac Rubio Ramos <rubio.b@pucp.edu.pe>
2025-11-29 01:44:02 -0500

    Update migrate_all_txt_modulo_almacenes.sql
    correcion

--- migrate_all_txt_modulo_almacenes.sql
+++ migrate_all_txt_modulo_almacenes.sql
@@ -37,12 +37,12 @@ BEGIN
 END
 GO
 -- Script to insert initial data into the 'Almacenes' table
-INSERT INTO Almacenes (id, nombre, ubicacion) VALUES
-(1, 'Principal', 'Norte'),
-(2, 'Secundario1', 'Sur'),
-(3, 'Secundario2', 'Este'),
-(4, 'Secundario3', 'Oeste'),
-(5, 'Secundario4', 'SurEste');
+INSERT INTO Almacenes (nombre, ubicacion) VALUES
+('Principal', 'Norte'),
+('Secundario1', 'Sur'),
+('Secundario2', 'Este'),
+('Secundario3', 'Oeste'),
+('Secundario4', 'SurEste');
 GO
 -- End of migration script for 'Almacenes' table
 
@@ -61,18 +61,17 @@ BEGIN
 END
 GO
 -- Script to insert initial data into the 'StockInsumos' table
-INSERT INTO StockInsumos (id, idInsumo, idAlmacen, stock, limiteBajo, limiteAlto) VALUES
-(1, 1, 1, 10, 10, 40),
-(2, 1, 2, 40, 5, 50),
-(3, 1, 3, 3, 5, 30),
-(4, 1, 4, 50, 10, 50),
-(5, 1, 4, 0, 10, 50),
-(6, 2, 1, 10, 1, 35),
-(7, 2, 2, 3, 3, 20),
-(8, 3, 1, 0, 5, 10),
-(9, 4, 1, 3, 5, 10),
-(10, 5, 1, 7, 5, 10),
-(11, 6, 1, 10, 5, 10);
+INSERT INTO StockInsumos (idInsumo, idAlmacen, stock, limiteBajo, limiteAlto) VALUES
+(1, 1, 10, 10, 40),
+(1, 2, 40, 5, 50),
+(1, 3, 3, 5, 30),
+(1, 4, 50, 10, 50),
+(1, 5, 0, 10, 50),
+(2, 1, 10, 1, 35),
+(2, 2, 3, 3, 20),
+(3, 1, 0, 5, 10),
+(4, 1, 3, 5, 10),
+(5, 1, 7, 5, 10),
+(6, 1, 10, 5, 10);
 GO
 -- End of migration script for 'StockInsumos' table
-
